2bf2dd435185a98201e826a6ba79b58ae1aa8367
hipoliciak <hipoliciak@gmail.com>
2025-04-13 20:20:03 +0200

    Slight change to ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dmodrzej <dmodrzej@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 22:01:39 by dmodrzej          #+#    #+#             */
-/*   Updated: 2025/04/13 20:11:56 by dmodrzej         ###   ########.fr       */
+/*   Updated: 2025/04/13 20:19:16 by dmodrzej         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,6 @@ int main(int argc, char** argv)
 	{
 		std::cerr << "Error: not enough arguments." << std::endl;
 		return 1;
-	}
-	for (int i = 1; i < argc; ++i)
-	{
-		std::string arg(argv[i]);
-		for (size_t j = 0; j < arg.length(); ++j)
-		{
-			if (!isdigit(arg[j]))
-			{
-				std::cerr << "Error: invalid character in input." << std::endl;
-				return 1;
-			}
-		}
-		long val = std::atol(arg.c_str());
-		if (val < 0 || val > INT_MAX)
-		{
-			std::cerr << "Error: invalid number." << std::endl;
-			return 1;
-		}
 	}
 	
     try
